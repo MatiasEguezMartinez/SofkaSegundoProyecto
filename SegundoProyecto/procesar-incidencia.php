8eacbb0f40e103580b8c2c58b89528ea89d2c553
MatiasEguezMartinez <100545070+MatiasEguezMartinez@users.noreply.github.com>
2022-11-04 00:39:26 -0300

    Add files via upload

---
+++ SegundoProyecto/procesar-incidencia.php
@@ -0,0 +1,8 @@
+<?php
+$Usuario = $_POST["User"];
+$Departemento = $_POST["Depto"];
+$Descripcion = $_POST["Descript"];
+$Passw = $_POST["Password"];
+
+echo "<b>Datos de incidencia</b></br></br>Usuario: ".$Usuario."</br>Departamento: ".$Departemento."</br>Descripción: ".$Descripcion."</br>Constraseña: ".$Passw;
+?>
